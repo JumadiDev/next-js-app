54522dc6bfa2b334a04c1ae939484d2c5d725cef
JumadiDev <jumadi.dev@gmail.com>
2024-06-22 02:11:20 -0600

    Barra de nanvegación base agregada

---
+++ src/app/components/header.js
@@ -0,0 +1,13 @@
+// components/Header.js
+const Header = () => {
+    return (
+    
+        <div className="container mx-auto text-center">
+          <h1 className="text-lg font-medium">Bienbenido a mi Web</h1> 
+        </div>
+       
+    );
+  };
+  
+  export default Header;
+  
