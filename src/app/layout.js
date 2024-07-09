0b6c37b547325e27b8fda21594d5fd37aba9bd76
JumadiDev <jumadi.dev@gmail.com>
2024-07-09 00:09:05 -0600

    Add  shadcn button

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Header from "./components/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
